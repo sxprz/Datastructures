939d27f648ca0673fbe8d4979b4d00f7c451af36
sxprz <derian.pljakic@gmail.com>
2022-03-08 23:12:37 +0100

    Add more documentation and notes

--- src/de/suprize/datastructures/RedBlackTree.java
+++ src/de/suprize/datastructures/RedBlackTree.java
@@ -1,7 +1,15 @@
 package de.suprize.datastructures;
 
 /**
- *  This is a custom implementation of the red-black-tree data structure.
+ *  This is a custom implementation of the red-black tree data structure.
+ *  A red-black tree follows these rules, when inserting a node:
+ *
+ *      1. A node is either red or black
+ *      2. The root and leaves (NIL) are black
+ *      3. If a node is red, then its children are black
+ *      4. All paths from a node to its NIL descendants contain the same number of black nodes
+ *
+ *  The following youtube link is the source for the above mentioned rules: https://youtu.be/5IBxA-bZZH8
  *
  * @author  suprize
  * @since   2022-03-03
@@ -22,7 +30,7 @@ public class RedBlackTree {
     private class RBNode {
         private final long val;
         Color color;
-        RBNode left, right;
+        RBNode left, right, parent;
 
         public RBNode(long val) {
             this.val = val;
@@ -34,19 +42,20 @@ public class RedBlackTree {
     }
 
     /**
-     *  search() looks for a value in the red-black-tree, which can be achieved in log(n) time,
+     *  search() looks for a value in the red-black tree, which can be achieved in log(n) time,
      *  where n is the number of nodes in the tree.
      *
      * @param  val the value that is being searched for
      * @return     returns a boolean value, whether the specified long value is found
      */
     public boolean search(long val) {
+        // TODO
         return false;
     }
 
     /**
      *  insert() will insert a value under the condition that the value does not
-     *  already exist in the red-black-tree.
+     *  already exist in the red-black tree.
      *
      * @param  val the value that needs to be inserted in the red-black-tree
      */
@@ -54,14 +63,18 @@ public class RedBlackTree {
         RBNode node = new RBNode(val);
         node.color = Color.RED;
 
-        RBNode temp = root;
+        RBNode parent, temp = root;
         while(temp != null) {
+            parent = temp.parent;
+            
             if(temp.val < val)
                 temp = temp.right;
             else if(temp.val > val)
                 temp = temp.left;
-            else
+            else {
+                // TODO: insert
                 return;
+            }
         }
     }
 
@@ -70,19 +83,20 @@ public class RedBlackTree {
      *  multiple values "at once". This method only exists for the user's comfort and will probably not lead
      *  to performance improvements over single calls of insert().
      *
-     * @param  vals a list of values that need to be inserted in the red-black-tree
+     * @param  vals a list of values that need to be inserted in the red-black tree
      */
     public void insertAll(long[] vals) {
-
+        // TODO
     }
 
     /**
-     *  delete() attempts to remove a node with a specific value.
+     *  delete() attempts to remove a node with a specific value in the red-black tree.
      *
      * @param  val the value that is being searched for
-     * @return     returns whether the node with the value 'val' in the red-black-tree was removed successfully.
+     * @return     returns whether the node with the value 'val' in the red-black tree was removed successfully.
      */
     public boolean delete(long val) {
+        // TODO
         return false;
     }
 }
